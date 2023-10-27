1f0e937a99a89b7e55c240eff0ee09c3925e13a7
Lazar Stanojevic <expbusinessit@gmail.com>
2023-10-27 09:43:23 +0200

    initial commit

---
+++ src/main/java/com/graphql/assignment/GraphQlAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.graphql.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graphql/assignment/GraphQlAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
