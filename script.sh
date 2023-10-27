dce93cc48709c4dc64e69775d51f7ccb94177628
Lazar Stanojevic <expbusinessit@gmail.com>
2023-10-27 20:45:04 +0200

    documentation

--- script.sh
+++ script.sh
@@ -3,10 +3,13 @@
 # Build WAR file using gradle
 ./gradlew clean build
 
+SOURCE="./build/libs/assignment-1.0.0.war"
+DEST="./assignment.war"
+
 # Check if build was successful.
 if [ $? -eq 0 ]; then
   echo "WAR file build successfully."
-
+  cp "$SOURCE" "$DEST"
   echo "Press any button to continue"
   read user_input
 else
